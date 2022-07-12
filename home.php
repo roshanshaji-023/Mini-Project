eaf81b4824263987e5594c7d2989e5cd7072cefa
roshan shaji <roshanshaji023@gmail.com>
2022-07-12 22:33:17 +0530

    .

--- home.php
+++ home.php
@@ -9,10 +9,12 @@ if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body >
     <div id="hello" class="d-flex justify-content-center align-items-center ">
     	
     	<div class="shadow w-450 p-3 text-center">
@@ -24,7 +26,12 @@ if (isset($_SESSION['id']) && isset($_SESSION['fname'])) {
     </div>
 	<section class="main">
 		<h3>Choose from below:</h3>
+	
         <div class="sample">
+		<div class="meta">
+              <img id="draw" src="Utilities/Images/undraw_workout_gcgu.svg" alt="">
+                <a href="more_features/bonemass.html">Bone Mass</a>
+            </div>
             <div class="bmi">
               <img id="draw" src="Utilities/Images/undraw_activity_tracker_re_2lvv.svg" alt="">
                 <a href="Utilities/bmi.html">BMI</a>
